6a050c0f8ebff861a41970222122df80ee9f1043
Mahfuj_SUST <107198635+mahfujalam77452@users.noreply.github.com>
2023-11-06 18:37:20 +0600

    Update Sparce table.cpp

--- Sparce table.cpp
+++ Sparce table.cpp
@@ -7,7 +7,6 @@ using namespace std;
 #define int long long
 const int N = 1e5 + 10;
 const int LOG=17;
-
 int a[N];
 int m[N][LOG];//here m[i][j] means the minimum number between i to 2^j;
 int bin_log[N];
@@ -17,6 +16,17 @@ int query(int L,int R){//log(1) time.
                           //but can't by log() function. Beacuse the log()
                           //function in c++ is e based.
     return min(m[L][k],m[R-(1<<k)+1][k]);
+}
+//This implementation for Range sum query.Time O(log(n)).
+int query_sum(int L, int R){
+    int sum=0;
+    for(int i=LOG-1;i>=0;i--){
+        if((1<<i<=(R-L+1))){
+            sum+=m[L][i];
+            L+=(1<<i);
+        }
+    }
+    return sum;
 }
 int32_t main()
 {
@@ -28,11 +38,18 @@ int32_t main()
        cin>>a[i];
        m[i][0]=a[i];
    }
+   
    for(int k=1;k<LOG;k++){//preprocessing O(N*log(N));
        for(int i=0;(i+(1<<k)-1)<n;i++){
            m[i][k]=min(m[i][k-1],m[i+(1<<(k-1))][k-1]);
        }
    }
+   /* If we want to find the Range sum query
+   for(int k=1;k<LOG;k++){//preprocessing O(N*log(N));
+       for(int i=0;(i+(1<<k)-1)<n;i++){
+           m[i][k]=m[i][k-1]+m[i+(1<<(k-1))][k-1];
+       }
+   }*/
    int q;
    cin>>q;
    for(int i=0;i<q;i++){
@@ -42,6 +59,3 @@ int32_t main()
    }
 	return 0;
 }
-
-
-
